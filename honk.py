515a39e8afba7830324b4726032d0ac8c5ea4e02
jaredculp <jculp14@gmail.com>
2022-10-26 13:14:39 -0400

    initial commit

---
+++ honk.py
@@ -0,0 +1,42 @@
+import os
+
+import enlighten
+import requests
+from bandcamp_dl.bandcamp import Bandcamp
+from bandcamp_dl.bandcampdownloader import BandcampDownloader
+from bs4 import BeautifulSoup
+
+base_url = "https://goosetheband.bandcamp.com"
+content = requests.get(base_url).content
+
+soup = BeautifulSoup(requests.get(base_url).content, "html.parser")
+urls = [
+    f"{base_url}{x['href']}" for x in soup.select("li.music-grid-item > a:nth-child(1)")
+]
+
+dl = BandcampDownloader(
+    template="%{artist}/%{album}/%{track} - %{title}",
+    directory=os.getcwd(),
+    overwrite=False,
+    embed_lyrics=False,
+    grouping=False,
+    embed_art=False,
+    no_slugify=False,
+    ok_chars="-_~",
+    space_char="-",
+    ascii_only=False,
+    keep_space=True,
+    keep_upper=True,
+    debugging=False,
+    urls=urls,
+)
+
+manager = enlighten.get_manager()
+pbar = manager.counter(total=len(urls))
+
+bc = Bandcamp()
+for url in urls:
+    dl.start(bc.parse(url))
+    pbar.update()
+
+manager.stop()
